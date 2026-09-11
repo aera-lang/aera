78af9984260d1a1d3594b2b6915fd50e83f37e92
anj0la <anjola.aina@gmail.com>
2026-09-10 22:11:55 -0600

    Update parser.ml
    - Fixed up call expression
    - Created array and tuple expressions and started working on the struct expression. Had to move grouping and tuple into a parse_paren_expr function

--- lib/frontend/parser.ml
+++ lib/frontend/parser.ml
@@ -95,15 +95,18 @@ let parse_identifier tok par =
 let rec parse_prefix op par =
     let (rhs, par') = par |> expr_bp (prefix_bp op) in (Unary ({op = op; rhs = rhs}), par')
 
-(* Grouping *)
+(* Grouping & Tuples *)
 
-and parse_grouping par =
-    let (expr, par') = par |> expr_bp 0 in (* get the grouping expr *)
+and parse_paren_expr par =
+    let (first, par') = par |> expr_bp 0 in 
     let tok = peek par' in 
     match tok.kind with 
-    | RightParen -> let (_, par'') = next par' in (Grouping expr, par'')
+    | RightParen -> let (_, par'') = next par' in (Grouping first, par'') (* parse grouping first *)
+    | Comma      -> let (_, par'') = next par' in par'' |> parse_tuple_expr first (* parse tuple *)
     | _          -> let par'' = report_error "expected ')' to close grouping" tok par' 
-                    in (Grouping expr, par'') (* still have a valid grouping, just not closed properly *)
+                    in (Grouping first, par'') (* still have a valid grouping, just not closed properly *)
+and parse_tuple_expr first par =
+    let (rest, par') = par |> parse_expr_list RightParen [] in (TupleExpr (first ::rest), par')
 
 (* Unrecognized Tokens - includes illegal tokens *)
 
@@ -119,11 +122,29 @@ and parse_unrecognized tok par =
 
 (* Call *)
 
-(* and parse_call lhs par =
+and parse_call lhs par =
     let (_, par') = next par in 
-    let (args, par'') = par' |> parse_args [] in
-    (Call {callee = lhs; args = args}, par'') *)
+    let (args, par'') = par' |>  parse_expr_list RightParen [] in
+    (Call {callee = lhs; args = args}, par'')
 
+and parse_expr_list closing_kind args par =
+    let tok = peek par in 
+    if tok.kind = closing_kind then 
+        let (_, par') = next par in (List.rev args, par')
+    else
+        let (arg, par') = par |> expr_bp 0 (* change to expr *) in 
+        let args' = (arg :: args) in 
+        let tok = peek par' in 
+        if tok.kind = Comma then 
+             let (_, par'') = next par' in par'' |> parse_expr_list closing_kind args'
+        else if tok.kind = closing_kind then 
+            par' |> parse_expr_list closing_kind args'
+        else
+            let par'' = report_error 
+            (Printf.sprintf "expected ',' or '%s' after parameter" (tok_to_string closing_kind)) 
+            tok par' 
+            in (List.rev args, par'')
+                           
 (* Binary & Assign *)
 
 and parse_binary op lhs min_bp par =
@@ -150,6 +171,25 @@ and parse_assign op lhs min_bp par =
             let (lhs'', par''') = par'' |> loop lhs' min_bp in 
             (lhs'', par''')
 
+(* Array *)
+and parse_array_expr par =
+    let (_, par') = next par in (* consume '[' token *)
+    let (rest, par'') = par' |> parse_expr_list RightBracket [] in (ArrayExpr rest, par'')
+
+(* Struct *)
+
+(* 
+
+struct_expression = identifier "{" { field_expression } "}" ;
+field_expression = identifier ":" expression ;
+*)
+
+(* and parse_struct_expr lhs par = 
+
+
+*) 
+(* Expression Without Block *)
+
 and expr_bp min_bp par = 
     let (tok, par') = next par in 
     let (lhs, par'') = match tok.kind with 
@@ -165,7 +205,7 @@ and expr_bp min_bp par =
     | Minus                     -> par' |> parse_prefix Neg
     | Exclaim                   -> par' |> parse_prefix Not
     (* Grouped Expression *)
-    | LeftParen                 -> par' |> parse_grouping
+    | LeftParen                 -> par' |> parse_paren_expr
     (* Invalid Token *)
     | _                         -> par' |> parse_unrecognized tok                       
     in
@@ -176,11 +216,28 @@ and loop lhs min_bp par =
     let tok = peek par in 
     match tok.kind with 
     | EOF                               -> (lhs, par)
-    (* Call Expressions *)
+    (* Call Expressions*)
     | LeftParen                         -> par |> parse_call lhs
     (* Binary / Assign Operators *)
     | op when is_binary_op tok.kind     -> par |> parse_binary (to_binary_op tok.kind) lhs min_bp
     | op when is_assign_op tok.kind     -> par |> parse_assign (to_assign_op tok.kind) lhs min_bp
+    (* Array *)
+    | LeftBracket                       -> par |> parse_array_expr
+    (* Struct *)
+    | LeftBrace                         -> par |> parse_struct_expr lhs
     | _                                 -> (lhs, par)
 
+(* Expression With Block *)
+
+and expr par =
+    let tok = peek par in 
+    match tok.kind with
+    | If -> let (_, par') = next par in par' |> if_expr
+    | While -> let (_, par') = next par in par' |> while_expr 
+    | Loop -> let (_, par') = next par in par' |> loop_expr
+    | Break -> let (_, par') = next par in par' |> break_expr
+    | Return -> let (_, par') = next par in par' |> return_expr
+    | _ -> par |> expr_bp 0 (* expression without block *)
+
+(* If Expr *)
 
--- lib/frontend/token.ml
+++ lib/frontend/token.ml
@@ -94,6 +94,101 @@ type token_kind =
 (* End Token *)
 | EOF
 
+let tok_to_string kind =
+    match kind with
+    (* Literals *)
+    | Identifier       -> "identifier"
+    | IntLiteral       -> "integer"
+    | FloatLiteral     -> "float"
+    | CharLiteral      -> "character"
+    | StringLiteral    -> "string"
+    | Unit             -> "unit"
+    (* Bool Keywords *)
+    | True             -> "true"
+    | False            -> "false"
+    (* Function / Statement Keywords *)
+    | Fn                -> "fn"
+    | Let               -> "let"
+    | Var               -> "var"
+    | Mut               -> "mut"
+    | Const             -> "const"
+    | Return            -> "return"
+    (* If / Loop / Match Keywords *)
+    | If                -> "if"
+    | Else              -> "else"
+    | For               -> "for"
+    | While             -> "while"
+    | Loop              -> "loop"
+    | Match             -> "match"
+    | In                -> "in"
+    | Break             -> "break"
+    (* User Type Keywords *)
+    | Struct             -> "struct"
+    | Variant            -> "variant"
+    | Use                -> "use"
+    (* Algebraic Effects Keywords *)
+    | Effect             -> "effect"
+    | Uses               -> "uses"
+    | With               -> "with"
+    | Do                 -> "do"
+    | Resume             -> "resume"
+    (* Other Keywords *)
+    | As                 -> "as"
+    (* Punctuation *)
+    | LeftParen          -> "("
+    | RightParen         -> ")"
+    | LeftBrace          -> "{"
+    | RightBrace         -> "}"
+    | LeftBracket        -> "["
+    | RightBracket       -> "]"
+    | Comma              -> ","
+    | Period             -> "."
+    | Colon              -> ":"
+    | LessLessEqual      -> "<<="
+    | GreaterGreaterEqual -> ">>="
+    | PeriodPeriodEqual  -> "..="
+    | AmpAmp             -> "&&"
+    | PipePipe           -> "||"
+    | EqualEqual         -> "=="
+    | ExclaimEqual       -> "!="
+    | LessEqual          -> "<="
+    | GreaterEqual       -> ">="
+    | LessLess           -> "<<"
+    | GreaterGreater     -> ">>"
+    | PlusEqual          -> "+="
+    | MinusEqual         -> "-="
+    | StarEqual          -> "*="
+    | SlashEqual         -> "/="
+    | PercentEqual       -> "%="
+    | AmpEqual           -> "&="
+    | PipeEqual          -> "|="
+    | CaretEqual         -> "^="
+    | MinusGreater       -> "->"
+    | PeriodPeriod       -> ".."
+    | EqualGreater       -> "=>"
+    | QuestionQuestion   -> "??"
+    | Amp                -> "&"
+    | Pipe               -> "|"
+    | Caret              -> "^"
+    | Tilde              -> "~"
+    | Plus               -> "+"
+    | Minus              -> "-"
+    | Star               -> "*"
+    | Slash              -> "/"
+    | Percent            -> "%"
+    | Question            -> "?"
+    | At                 -> "@"
+    | Exclaim             -> "!"
+    | Less                -> "<"
+    | Greater             -> ">"
+    | Equal               -> "="
+    (* Comments *)
+    | LineComment        -> "#"
+    (* Error *)
+    | Illegal            -> "illegal token"
+    (* End Token *)
+    | EOF                -> "end of file"
+
 type t = {
     kind: token_kind;
     span: Span.t;
