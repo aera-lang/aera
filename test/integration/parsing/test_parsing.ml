879320a0e1028f1db3abe3eb11249eee697efa07
anj0la <anjola.aina@gmail.com>
2026-09-26 08:16:40 -0600

    Add valid program tests
    - Created tests to test out the valid programs I created. I will continue to add more valid tests as the language grows. For now, the next thing is error recovery and testing out invalid programs, then we are FINALLY done with the parser (for now).

--- test/integration/parsing/test_parsing.ml
+++ test/integration/parsing/test_parsing.ml
@@ -4,18 +4,14 @@ open Alcotest
 (* -------------------- Helper Functions -------------------- *)
 
 let read_file path =
-    let contents = In_channel.with_open_bin path In_channel.input_all in
-    match contents with 
+    match In_channel.with_open_bin path In_channel.input_all with
     | contents                  -> Ok (contents, Filename.basename path) (* input, filename *)
     | exception Sys_error msg   -> Error (msg)
 
-let parse_source path = 
-    match read_file path with 
-    | Ok (input, filename) -> 
-        let src = Source.create input filename in 
-        let lex = Lexer.read_tokens (Lexer.create src []) in 
-        Parser.parse (Parser.create src lex.tokens [])
-    | Error msg -> failwith msg
+let parse_source input = 
+    let src = Source.create input "fixture.aera" in 
+    let lex = Lexer.read_tokens (Lexer.create src []) in 
+    Parser.parse (Parser.create src lex.tokens [])
 
 let pp_item fmt item =
     Format.fprintf fmt "%s" (String.concat " " (Pretty.walk_item item))
@@ -28,7 +24,36 @@ let check_round_trip msg input =
     let (ast2, _) = parse_source printed in
     Alcotest.(check (list item_testable)) msg ast1 ast2
 
+let test_fixture name = 
+    let path = "../../fixtures/" ^ name in 
+    match read_file path with 
+    | Ok (src, filename) -> 
+        check_round_trip name src
+    | Error msg -> failwith msg
+
 (* -------------------- Valid Program Tests -------------------- *)
 
+let test_basic_program () = test_fixture "parsing/valid/basic.aera"
+let test_closures () = test_fixture "parsing/valid/basic.aera"
+let test_collections () = test_fixture "parsing/valid/basic.aera"
+let test_functions () = test_fixture "parsing/valid/basic.aera"
+let test_structs () = test_fixture "parsing/valid/basic.aera"
 
-(* -------------------- Invalid Program Tests -------------------- *)
+
+(* -------------------- Program Tests -------------------- *)
+
+
+let tests = [
+    ("valid programs", [
+        Alcotest.test_case "basic program" `Quick test_basic_program;
+        Alcotest.test_case "closures" `Quick test_closures;
+        Alcotest.test_case "collections (arrays / tuples)" `Quick test_collections;
+        Alcotest.test_case "funuctions" `Quick test_functions;
+        Alcotest.test_case "structs" `Quick test_structs;
+    ]);
+]
+
+(* -------------------- Driver Function -------------------- *)
+
+let () =
+    Alcotest.run "Parsing" tests
